9d8908d0b1453837ac502427711cf1e69fef5ed9
Devendra-2021 <brainmuscle0147@gmail.com>
2021-05-17 12:31:37 +0530

    Updates

---
+++ precache-manifest.7436829e72243ff78db6652ee72aa941.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8ab187796593c0439f09f1d79c8f965",
+    "url": "/expense-tracker/index.html"
+  },
+  {
+    "revision": "1f0f3d9d777dbba4d2c6",
+    "url": "/expense-tracker/static/css/main.708a4d9b.chunk.css"
+  },
+  {
+    "revision": "bd6bd55d0f17c3088198",
+    "url": "/expense-tracker/static/js/2.b190f742.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/expense-tracker/static/js/2.b190f742.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f0f3d9d777dbba4d2c6",
+    "url": "/expense-tracker/static/js/main.4a5280d6.chunk.js"
+  },
+  {
+    "revision": "7284d3afe7f27382a2ae",
+    "url": "/expense-tracker/static/js/runtime-main.ace402c5.js"
+  }
+]);
